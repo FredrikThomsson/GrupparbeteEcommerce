9104c210c44af8a11655e5e594bb679117461128
Fredrik Thomsson <fredrikthomsson1@gmail.com>
2023-05-02 19:55:02 +0200

    first commit

---
+++ Grupparbetet/GrupparbeteMVC/Data/AppDbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using GrupparbeteMVC.Models;
+
+namespace GrupparbeteMVC.Data
+
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder) 
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<ApplicationContext>();
+
+                context.Database.EnsureCreated();
+
+                if (!context.Products.Any())
+                {
+                    context.Products.AddRange(new List<Product>()
+                    {
+                        new Product()
+                        {
+                            Name = "Test",
+                            Description= "Test",
+                            SEKPrice = 100,
+                            DollarPrice= 10,
+                        }
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+
+
+}
+
